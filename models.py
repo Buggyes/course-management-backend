bd0e42176b32ddd7113d1a1a9f011aa100cc3ffe
Buggyes <matheus@chatisthisreal>
2025-06-02 09:14:07 -0300

    finished defining the models

--- models.py
+++ models.py
@@ -1,23 +1,37 @@
 from sqlmodel import Field, SQLModel
 
-class Instructor(SQLModel, table=True):
+class Role(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
-    biography: str = Field(default=None)
-    pfp: bytearray = Field(default=None)
-    banner: bytearray = Field(default=None)
+    name: str = Field(default=None, index=True)
 
 class AreaAction(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-
-class InstructorArea(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    instructor_id: int | None = Field(default=None, foreign_key="instructor.id")
-    area_id: int | None = Field(default=None, foreign_key="areaaction.id")
+    name: str = Field(default=None, index=True)
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     login: str = Field(default=None, index=True)
     password: str = Field(default=None, index=True)
-    role_id: int | None = Field(default=None, foreign_key="areaaction.id")
+    role_id: int | None = Field(default=None, foreign_key="role.id")
+
+class Instructor(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    biography: str | None = None
+    pfp: bytearray | None = None
+    banner: bytearray | None = None
+    user_id: int | None = Field(default=None, foreign_key="user.id")
+
+class Course(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(default=None)
+    description: str = Field(default=None)
+    video: bytearray | None = None
+    banner: bytearray | None = None
+    instructor_id: int | None = Field(default=None, foreign_key="instructor.id")
+
+
+class InstructorArea(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    instructor_id: int | None = Field(default=None, foreign_key="instructor.id")
+    area_id: int | None = Field(default=None, foreign_key="areaaction.id")
